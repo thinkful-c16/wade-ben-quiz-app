5569bed5563dc753b27790dc967597b14bd433e3
Ben Parman <benparman@gmail.com>
2017-11-07 10:39:06 -0700

    Completed answer-view generate function and updated store

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 // In-memory database of questions, answers, and correct answer
 
-const QUIZDATA = [
+const QUESTIONS = [
   {
     question: 'Who is the supreme lord of the nine hells?',
     answers: ['Asmodeus', 'Mephistopholes', 'Richard Greenhill'],
@@ -32,68 +32,68 @@ const QUIZDATA = [
 
 // Create your initial store
 const STORE = {
-  // Current Question
+  // Current Question Index
+  'current question': 0,
+  // Current Question counter
+  'current counter': 1,
   // User's answer choice(s)
   // Current view
   // Score? Anything else?
-
+  'score': 0,
 };
 
-function handleWelcomeView() {
+function handleUserInputs() {
   // listener for begin button to trigger rendering of first question
   $('#quiz-start-button').on('click', event => {
-    let currentQuestionNumberIndex = 0;
-    renderQuestionView(currentQuestionNumberIndex);
+    renderQuestionView();
   });
 }
 
 // Template generators
+function generateAnswerView() {
+  let questionIndex = STORE['current question'];
+  let answers = QUESTIONS[questionIndex].answers;
+
+  return 
+    `<div>
+      <h1>Infernal Plane Quiz</h1>
+      <div class= 'questions-answered'>
+          <p>Question ${STORE['current counter']}/5</p>
+      </div>
+      <form>
+          <h3>${QUESTIONS[STORE['current question']].question}</h3>
+          <div>
+              <input type="radio" id="${answers[0]}"
+              name="answer" value="${answers[0]}">
+              <label for="${answers[0]}">${answers[0]}</label>
+              <br>
+              <input type="radio" id="${answers[1]}"
+              name="answer" value="${answers[1]}">
+              <label for="${answers[1]}">${answers[1]}</label>
+              <br>
+              <input type="radio" id="${answers[2]}"
+              name="answer" value="${answers[2]}">
+              <label for="${answers[2]}">${answers[2]}</label>
+          </div>
+          <div class="user-input">
+              <button name= "submit-button" id= "answer-submit-button" class= "input-button" type= "submit" >Submit Answer</button>
+          </div>
+          <div class= "current-score">
+              <p>Current score: ${STORE['score']}/5</p>
+          </div>
+      </form>
+    </div>`
+  ;
+}
 // function generateAnswerList(answers) {}
 
 
 // Rendering functions
-function renderQuestionView(currentIndex) {
-  // Definition of local variables for the purpose of accessing contents of QUIZDATA
-  let displayedQuestionNumber = currentIndex;
-  displayedQuestionNumber++;
-  let currentQuestion = QUIZDATA[currentIndex];
-  let question = currentQuestion.question;
-  let answerOne = currentQuestion.answers[0];
-  let answerTwo = currentQuestion.answers[1];
-  let answerThree = currentQuestion.answers[2];
+function renderQuestionView() {
+  // Definition of local variables for the purpose of accessing contents of QUESTIONS
 
   // inserting Question Template into the DOM
-  $('.container').html(
-    `<div>
-  <h1>Infernal Plane Quiz</h1>
-  <div class= 'questions-answered'>
-      <p>Question ${displayedQuestionNumber}/5</p>
-  </div>
-  <form>
-      <h3>${question}</h3>
-      <div>
-          <input type="radio" id="${answerOne}"
-          name="answer" value="${answerOne}">
-          <label for="${answerOne}">${answerOne}</label>
-          <br>
-          <input type="radio" id="${answerTwo}"
-          name="answer" value="${answerTwo}">
-          <label for="${answerTwo}">${answerTwo}</label>
-          <br>
-          <input type="radio" id="${answerThree}"
-          name="answer" value="${answerThree}">
-          <label for="${answerThree}">${answerThree}</label>
-      </div>
-      <div class="user-input">
-          <button name= "submit-button" id= "answer-submit-button" class= "input-button" type= "submit" >Submit Answer</button>
-      </div>
-      <div class= "current-score">
-          <p>Current score: 0/5</p>
-      </div>
-  </form>
-</div>`
-  );
-  handleAnswerSubmitted();
+  generateAnswerView();
 }
 
 // Event handlers
@@ -106,6 +106,6 @@ function handleAnswerSubmitted() {
 }
 
 $(function main() {
-  handleWelcomeView();
+  handleUserInputs();
   handleAnswerSubmitted();
 });
